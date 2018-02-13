8ff4dc36a06bff434d1a8017e9c874fe87ad6cb5
Javier Díez García <javi_codema@hotmail.com>
2018-02-13 19:46:05 +0100

    [WIP] Started working on new requirements
    I've created a branch to work on it until we have all the new
    requirements implemented and tested (to avoid problems)
    
    Fixed UserInfo and UserInfoAdapter working on the JSON response issue
    (work in progress)

--- src/main/java/domain/UserInfoAdapter.java
+++ src/main/java/domain/UserInfoAdapter.java
@@ -6,25 +6,24 @@ import java.time.ZoneId;
 import java.util.Date;
 
 /**
- * Created by Nicolás on 15/02/2017.
- * Class in charge of translating a User object into the response format
- * Note: this class only creates a model class that contains a subset of the fields in the User class
+ * Created by Nicolas on 15/02/2017. Class in charge of translating a User
+ * object into the response format Note: this class only creates a model class
+ * that contains a subset of the fields in the User class
+ * Modified by Javier on 13/02/2018 to match the new requirements.
  */
 public class UserInfoAdapter {
 
-    private User user;
+	private User user;
 
-    public UserInfoAdapter(User user) {
-        this.user = user;
-    }
+	public UserInfoAdapter(User user) {
+		this.user = user;
+	}
 
-    public UserInfo userToInfo(){
-        LocalDate current = new Date().toInstant()
-                .atZone(ZoneId.systemDefault()).toLocalDate();
-        LocalDate then = user.getDateOfBirth().toInstant()
-                .atZone(ZoneId.systemDefault()).toLocalDate();
-        Period per = Period.between(then, current);
-        return new UserInfo(user.getFirstName(), user.getLastName(),
-                per.getYears(), user.getUserId(), user.getEmail());
-    }
+	public UserInfo userToInfo() {
+		LocalDate current = new Date().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+		LocalDate then = user.getDateOfBirth().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+		Period per = Period.between(then, current);
+		return new UserInfo(user.getName(), user.getLocation(), user.getEmail(), user.getUserId(),
+				user.getKind(), user.getKindCode());
+	}
 }
